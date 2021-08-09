5a0ef5cb8c93c678c1df2e5f6deb9aab90f55700
LeymanMA <leymanma@antereal.ru>
2021-08-09 15:58:26 +0700

    text texture holding in structure was added

--- examples/main_window.rs
+++ examples/main_window.rs
@@ -6,10 +6,11 @@ fn sum() {
 }
 
 fn main() {
-    let btn = Button::new("Start", 120, 40, 20, 10, Some(Box::new(sum)));
-    let btn2 = Button::new("End", 120, 40, 160, 10, None);
-
     let mut main_window = init("Fantasy World", 800, 600);
+
+    let btn = Button::new("Start", 120, 40, 20, 10, &main_window.canvas, Some(Box::new(sum)));
+    let btn2 = Button::new("End", 120, 40, 160, 10, &main_window.canvas, None);
+
     main_window.add_child(Box::new(btn));
     main_window.add_child(Box::new(btn2));
 
--- src/buttons.rs
+++ src/buttons.rs
@@ -1,3 +1,5 @@
+use sdl2::render::Texture;
+use sdl2::sys::SDL_Texture;
 use sdl2::{rect::{Point, Rect}, render::Canvas};
 use sdl2::video::Window;
 use sdl2::pixels::Color;
@@ -14,12 +16,26 @@ pub struct Button {
 
     pub text: String,
     pub text_wrapper: Rect,
+    pub text_texture: *mut SDL_Texture,
 
     on_click_fn: Option<Box<dyn FnMut() -> ()>>
 }
 
 impl Button {
-    pub fn new(text: &str, width: u32, height: u32, x: i32, y: i32, on_click_fn: Option<Box<dyn FnMut() -> ()>>) -> Button {
+    pub fn new(text: &str, width: u32, height: u32, x: i32, y: i32, canvas: &Canvas<Window>, on_click_fn: Option<Box<dyn FnMut() -> ()>>) -> Button {
+        let ttf_context = sdl2::ttf::init().unwrap();
+    
+        let mut font = ttf_context.load_font("fonts\\Default.ttf", 64).unwrap();
+        font.set_style(sdl2::ttf::FontStyle::NORMAL);
+        
+        let surface = font
+            .render(text)
+            .blended(Color::RGBA(130, 80, 130, 255))
+            .unwrap();
+
+        let texture_creator = canvas.texture_creator();
+        let texture = texture_creator.create_texture_from_surface(surface).unwrap();
+
         Button {
             border: Rect::new(x, y, width, height),
             inner: Rect::new(x+2, y+2, width-4, height-4),
@@ -28,6 +44,7 @@ impl Button {
             
             text: text.to_string(),
             text_wrapper: Rect::new(x+20, y+5, width-40, height-10),
+            text_texture: texture.raw(),
             
             on_click_fn,
         }
@@ -41,29 +58,23 @@ impl Button {
         self.text = String::from(new_text)
     }
 
+    fn get_text_texture(&self, canvas: &Canvas<Window>) -> Texture {
+        unsafe {
+            canvas.raw_create_texture(self.text_texture)
+        }
+    }
 }
 
 impl Drawaible for Button {
     fn draw(&self, canvas: &mut Canvas<Window>) {
         // ToDo: Переделать отрисовку текста, текстура не создавалась заново каждый раз
-        let ttf_context = sdl2::ttf::init().unwrap();
-    
-        let mut font = ttf_context.load_font("fonts\\Default.ttf", 64).unwrap();
-        font.set_style(sdl2::ttf::FontStyle::NORMAL);
-        
-        let surface = font
-            .render(self.text.as_str())
-            .blended(Color::RGBA(130, 80, 130, 255))
-            .unwrap();
-
-        let texture_creator = canvas.texture_creator();
-        let texture = texture_creator.create_texture_from_surface(surface).unwrap();
 
         canvas.set_draw_color(Color::RGB(211, 167, 177));
         canvas.draw_rect(self.border).unwrap();
         canvas.set_draw_color(Color::RGB(200, 162, 200));
         canvas.fill_rect(self.inner).unwrap(); 
-
+        
+        let texture = self.get_text_texture(&canvas);
         canvas.copy(&texture, None, Some(self.text_wrapper)).unwrap();
     }
 }
